7f258a647076a336eb7dd11843e293535cf4bf96
Victor Pereira <victor.m.s.pereira.95@gmail.com>
2021-11-10 11:47:25 -0300

    Linhas e Colunas

--- src/application/SistemaXadrez.java
+++ src/application/SistemaXadrez.java
@@ -1,11 +1,17 @@
 package application;
 
+import java.util.Scanner;
+
 public class SistemaXadrez {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("Bora jogar xadrez?");
+		int l = sc.nextInt();
+		int c = sc.nextInt();
 
+		System.out.println("Posição: " + l +", " + c);
 	}
-
 }
