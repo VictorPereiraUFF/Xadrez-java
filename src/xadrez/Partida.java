0928a5254f0de3dbe5d1efa1c2b2fa9718db750c
Victor Pereira <victor.m.s.pereira.95@gmail.com>
2021-12-06 17:59:10 -0300

    Correção da posição

--- src/xadrez/Partida.java
+++ src/xadrez/Partida.java
@@ -1,7 +1,8 @@
 package xadrez;
 
+import boardgame.LinhasEColunas;
 import boardgame.Tabuleiro;
-import xadrez.pecas.*;
+import xadrez.pecas.Peão;
 
 public class Partida {
 	
@@ -9,22 +10,27 @@ public class Partida {
 	
 	public Partida() {
 		tabuleiro = new Tabuleiro (8, 8);
+		setupInicial();
 	}
 	
+	
+
 	public XadrezPecas[][] getPecas() {
 		XadrezPecas [][] mat = new XadrezPecas[tabuleiro.getLinhas()][tabuleiro.getColunas()];
 		for (int i=0; i<tabuleiro.getLinhas(); i++) {
 			for (int j=0; j<tabuleiro.getColunas(); j++) {
 				mat[i][j] = (XadrezPecas) tabuleiro.pecas(i,j);
+					
+				}
 			}
+			return mat;
 		}
-		return mat;
 		
 		private void setupInicial() {
-			tabuleiro.placePiece(new Peao(tabuleiro, color.BRANCO), new Posicao(2, 6));
-		 }
-	}
-	
+			tabuleiro.PosicaoPeca(new Peão(tabuleiro, XadrezJogador.BRANCO), new LinhasEColunas(2, 6));
+			
+		}
+
 
 		
 
