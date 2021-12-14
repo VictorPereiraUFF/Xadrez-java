ec19f7936ebc99aba757c79710d5d602f575a052
Victor Pereira <victor.m.s.pereira.95@gmail.com>
2021-12-14 18:46:25 -0300

    Ambos os jogadores

--- src/xadrez/Partida.java
+++ src/xadrez/Partida.java
@@ -1,13 +1,12 @@
 package xadrez;
 
-import boardgame.LinhasEColunas;
 import boardgame.Tabuleiro;
-import xadrez.pecas.Peão;
-import xadrez.pecas.Rei;
-import xadrez.pecas.Rainha;
-import xadrez.pecas.Torre;
-import xadrez.pecas.Cavalo;
 import xadrez.pecas.Bispo;
+import xadrez.pecas.Cavalo;
+import xadrez.pecas.Peão;
+import xadrez.pecas.Rainha;
+import xadrez.pecas.Rei;
+import xadrez.pecas.Torre;
 
 public class Partida {
 	
@@ -35,15 +34,22 @@ public class Partida {
 	
 	
 		private void setupInicial() {
-			tabuleiro.PosicaoPeca(new Peão(tabuleiro, XadrezJogador.BRANCO), new LinhasEColunas(2, 6));
-			tabuleiro.PosicaoPeca(new Rei(tabuleiro, XadrezJogador.BRANCO), new LinhasEColunas(7, 3));
-			tabuleiro.PosicaoPeca(new Rainha(tabuleiro, XadrezJogador.BRANCO), new LinhasEColunas(7, 4));
-			tabuleiro.PosicaoPeca(new Torre(tabuleiro, XadrezJogador.BRANCO), new LinhasEColunas(7, 0));
-			tabuleiro.PosicaoPeca(new Torre(tabuleiro, XadrezJogador.BRANCO), new LinhasEColunas(7, 7));
-			tabuleiro.PosicaoPeca(new Bispo(tabuleiro, XadrezJogador.BRANCO), new LinhasEColunas(7, 2));
-			tabuleiro.PosicaoPeca(new Bispo(tabuleiro, XadrezJogador.BRANCO), new LinhasEColunas(7, 5));
-			tabuleiro.PosicaoPeca(new Cavalo(tabuleiro, XadrezJogador.BRANCO), new LinhasEColunas(7, 1));
-			tabuleiro.PosicaoPeca(new Cavalo(tabuleiro, XadrezJogador.BRANCO), new LinhasEColunas(7, 6));
+			posicaoPecaNova('d', 1, new Rei(tabuleiro, XadrezJogador.BRANCO));
+			posicaoPecaNova('e', 1, new Rainha(tabuleiro, XadrezJogador.BRANCO));
+			posicaoPecaNova('a', 1, new Torre(tabuleiro, XadrezJogador.BRANCO));
+			posicaoPecaNova('h', 1, new Torre(tabuleiro, XadrezJogador.BRANCO));
+			posicaoPecaNova('b', 1, new Cavalo(tabuleiro, XadrezJogador.BRANCO));
+			posicaoPecaNova('g', 1, new Cavalo(tabuleiro, XadrezJogador.BRANCO));
+			posicaoPecaNova('c', 1, new Bispo(tabuleiro, XadrezJogador.BRANCO));
+			posicaoPecaNova('f', 1, new Bispo(tabuleiro, XadrezJogador.BRANCO));
+			posicaoPecaNova('d', 8, new Rei(tabuleiro, XadrezJogador.PRETO));
+			posicaoPecaNova('e', 8, new Rainha(tabuleiro, XadrezJogador.PRETO));
+			posicaoPecaNova('a', 8, new Torre(tabuleiro, XadrezJogador.PRETO));
+			posicaoPecaNova('h', 8, new Torre(tabuleiro, XadrezJogador.PRETO));
+			posicaoPecaNova('b', 8, new Cavalo(tabuleiro, XadrezJogador.PRETO));
+			posicaoPecaNova('g', 8, new Cavalo(tabuleiro, XadrezJogador.PRETO));
+			posicaoPecaNova('c', 8, new Bispo(tabuleiro, XadrezJogador.PRETO));
+			posicaoPecaNova('f', 8, new Bispo(tabuleiro, XadrezJogador.PRETO));
 
 
 
