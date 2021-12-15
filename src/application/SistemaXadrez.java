16ca62dbca732526bbe6cbc7e5a57b92f3044b1f
Victor Pereira <victor.m.s.pereira.95@gmail.com>
2021-12-15 12:53:07 -0300

    Movimentos das peças

--- src/application/SistemaXadrez.java
+++ src/application/SistemaXadrez.java
@@ -3,25 +3,37 @@ package application;
 import java.util.Scanner;
 
 import xadrez.Partida;
+import xadrez.XadrezPecas;
+import xadrez.XadrezPosicao;
 
 public class SistemaXadrez {
 
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
+				
 		System.out.println("Bora jogar xadrez?");
 		System.out.println();
-
+		
+		Scanner sc = new Scanner(System.in);
 		Partida partida = new Partida();
-		Interface.printTabuleiro(partida.getPecas());
+		
+		while (true) {
+			Interface.printTabuleiro(partida.getPecas());
+			System.out.println();
+			System.out.print("Posição de origem: ");
+			XadrezPosicao origem = Interface.lerXadrezPosicao(sc); 
+			System.out.println();
+			System.out.print("Posição de destino: ");
+			XadrezPosicao destino = Interface.lerXadrezPosicao(sc); 
+			
+			XadrezPecas pecaCapturada = partida.moverPeca(origem, destino);
+		}
 		
 		
 		
 		
 		
 		
-		sc.close();
+		
 		
 		
 		}
--- src/xadrez/Partida.java
+++ src/xadrez/Partida.java
@@ -1,5 +1,7 @@
 package xadrez;
 
+import boardgame.LinhasEColunas;
+import boardgame.Pecas;
 import boardgame.Tabuleiro;
 import xadrez.pecas.Bispo;
 import xadrez.pecas.Cavalo;
@@ -9,66 +11,83 @@ import xadrez.pecas.Rei;
 import xadrez.pecas.Torre;
 
 public class Partida {
-	
+
 	private Tabuleiro tabuleiro;
-	
+
 	public Partida() {
-		tabuleiro = new Tabuleiro (8, 8);
+		tabuleiro = new Tabuleiro(8, 8);
 		setupInicial();
 	}
 
 	public XadrezPecas[][] getPecas() {
-		XadrezPecas [][] mat = new XadrezPecas[tabuleiro.getLinhas()][tabuleiro.getColunas()];
-		for (int i=0; i<tabuleiro.getLinhas(); i++) {
-			for (int j=0; j<tabuleiro.getColunas(); j++) {
-				mat[i][j] = (XadrezPecas) tabuleiro.pecas(i,j);
-					
-				}
+		XadrezPecas[][] mat = new XadrezPecas[tabuleiro.getLinhas()][tabuleiro.getColunas()];
+		for (int i = 0; i < tabuleiro.getLinhas(); i++) {
+			for (int j = 0; j < tabuleiro.getColunas(); j++) {
+				mat[i][j] = (XadrezPecas) tabuleiro.pecas(i, j);
+
 			}
-			return mat;
 		}
-		
-		private void posicaoPecaNova (char coluna, int linha, XadrezPecas pecas) {
-			tabuleiro.PosicaoPeca(pecas, new XadrezPosicao(coluna, linha).toPosicao());
-		}
-	
-	
-		private void setupInicial() {
-			posicaoPecaNova('d', 1, new Rei(tabuleiro, XadrezJogador.BRANCO));
-			posicaoPecaNova('e', 1, new Rainha(tabuleiro, XadrezJogador.BRANCO));
-			posicaoPecaNova('a', 1, new Torre(tabuleiro, XadrezJogador.BRANCO));
-			posicaoPecaNova('h', 1, new Torre(tabuleiro, XadrezJogador.BRANCO));
-			posicaoPecaNova('b', 1, new Cavalo(tabuleiro, XadrezJogador.BRANCO));
-			posicaoPecaNova('g', 1, new Cavalo(tabuleiro, XadrezJogador.BRANCO));
-			posicaoPecaNova('c', 1, new Bispo(tabuleiro, XadrezJogador.BRANCO));
-			posicaoPecaNova('f', 1, new Bispo(tabuleiro, XadrezJogador.BRANCO));
-			posicaoPecaNova('d', 2, new Peão(tabuleiro, XadrezJogador.BRANCO));
-			posicaoPecaNova('e', 2, new Peão(tabuleiro, XadrezJogador.BRANCO));
-			posicaoPecaNova('a', 2, new Peão(tabuleiro, XadrezJogador.BRANCO));
-			posicaoPecaNova('h', 2, new Peão(tabuleiro, XadrezJogador.BRANCO));
-			posicaoPecaNova('b', 2, new Peão(tabuleiro, XadrezJogador.BRANCO));
-			posicaoPecaNova('g', 2, new Peão(tabuleiro, XadrezJogador.BRANCO));
-			posicaoPecaNova('c', 2, new Peão(tabuleiro, XadrezJogador.BRANCO));
-			posicaoPecaNova('f', 2, new Peão(tabuleiro, XadrezJogador.BRANCO));
-			posicaoPecaNova('d', 8, new Rei(tabuleiro, XadrezJogador.PRETO));
-			posicaoPecaNova('e', 8, new Rainha(tabuleiro, XadrezJogador.PRETO));
-			posicaoPecaNova('a', 8, new Torre(tabuleiro, XadrezJogador.PRETO));
-			posicaoPecaNova('h', 8, new Torre(tabuleiro, XadrezJogador.PRETO));
-			posicaoPecaNova('b', 8, new Cavalo(tabuleiro, XadrezJogador.PRETO));
-			posicaoPecaNova('g', 8, new Cavalo(tabuleiro, XadrezJogador.PRETO));
-			posicaoPecaNova('c', 8, new Bispo(tabuleiro, XadrezJogador.PRETO));
-			posicaoPecaNova('f', 8, new Bispo(tabuleiro, XadrezJogador.PRETO));
-			posicaoPecaNova('d', 7, new Peão(tabuleiro, XadrezJogador.PRETO));
-			posicaoPecaNova('e', 7, new Peão(tabuleiro, XadrezJogador.PRETO));
-			posicaoPecaNova('a', 7, new Peão(tabuleiro, XadrezJogador.PRETO));
-			posicaoPecaNova('h', 7, new Peão(tabuleiro, XadrezJogador.PRETO));
-			posicaoPecaNova('b', 7, new Peão(tabuleiro, XadrezJogador.PRETO));
-			posicaoPecaNova('g', 7, new Peão(tabuleiro, XadrezJogador.PRETO));
-			posicaoPecaNova('c', 7, new Peão(tabuleiro, XadrezJogador.PRETO));
-			posicaoPecaNova('f', 7, new Peão(tabuleiro, XadrezJogador.PRETO));
-		}
+		return mat;
+	}
 
+	public XadrezPecas moverPeca(XadrezPosicao posicaoOrigem, XadrezPosicao posicaoDestino) {
+		LinhasEColunas origem = posicaoOrigem.toPosicao();
+		LinhasEColunas destino = posicaoDestino.toPosicao();
+		validarPosicaoOrigem(origem);
+		Pecas pecaCapturada = movimento(origem, destino);
+		return (XadrezPecas) pecaCapturada;
+	}
+	
+	private Pecas movimento (LinhasEColunas origem, LinhasEColunas destino) {
+		Pecas p = tabuleiro.removerPeca(origem);
+		Pecas pecaCapturada = tabuleiro.removerPeca(destino);
+		tabuleiro.PosicaoPeca(p, destino);
+		return pecaCapturada;
+	}
+	
+	private void validarPosicaoOrigem(LinhasEColunas posicao) {
+		if (!tabuleiro.posicaoComPeca(posicao)) {
+			throw new XadrezException("Erro: a posição de origem declarada está vazia");
+		}
+	}
 
-		
+	private void posicaoPecaNova(char coluna, int linha, XadrezPecas pecas) {
+		tabuleiro.PosicaoPeca(pecas, new XadrezPosicao(coluna, linha).toPosicao());
+	}
+
+	private void setupInicial() {
+		posicaoPecaNova('d', 1, new Rei(tabuleiro, XadrezJogador.BRANCO));
+		posicaoPecaNova('e', 1, new Rainha(tabuleiro, XadrezJogador.BRANCO));
+		posicaoPecaNova('a', 1, new Torre(tabuleiro, XadrezJogador.BRANCO));
+		posicaoPecaNova('h', 1, new Torre(tabuleiro, XadrezJogador.BRANCO));
+		posicaoPecaNova('b', 1, new Cavalo(tabuleiro, XadrezJogador.BRANCO));
+		posicaoPecaNova('g', 1, new Cavalo(tabuleiro, XadrezJogador.BRANCO));
+		posicaoPecaNova('c', 1, new Bispo(tabuleiro, XadrezJogador.BRANCO));
+		posicaoPecaNova('f', 1, new Bispo(tabuleiro, XadrezJogador.BRANCO));
+		posicaoPecaNova('d', 2, new Peão(tabuleiro, XadrezJogador.BRANCO));
+		posicaoPecaNova('e', 2, new Peão(tabuleiro, XadrezJogador.BRANCO));
+		posicaoPecaNova('a', 2, new Peão(tabuleiro, XadrezJogador.BRANCO));
+		posicaoPecaNova('h', 2, new Peão(tabuleiro, XadrezJogador.BRANCO));
+		posicaoPecaNova('b', 2, new Peão(tabuleiro, XadrezJogador.BRANCO));
+		posicaoPecaNova('g', 2, new Peão(tabuleiro, XadrezJogador.BRANCO));
+		posicaoPecaNova('c', 2, new Peão(tabuleiro, XadrezJogador.BRANCO));
+		posicaoPecaNova('f', 2, new Peão(tabuleiro, XadrezJogador.BRANCO));
+		posicaoPecaNova('d', 8, new Rei(tabuleiro, XadrezJogador.PRETO));
+		posicaoPecaNova('e', 8, new Rainha(tabuleiro, XadrezJogador.PRETO));
+		posicaoPecaNova('a', 8, new Torre(tabuleiro, XadrezJogador.PRETO));
+		posicaoPecaNova('h', 8, new Torre(tabuleiro, XadrezJogador.PRETO));
+		posicaoPecaNova('b', 8, new Cavalo(tabuleiro, XadrezJogador.PRETO));
+		posicaoPecaNova('g', 8, new Cavalo(tabuleiro, XadrezJogador.PRETO));
+		posicaoPecaNova('c', 8, new Bispo(tabuleiro, XadrezJogador.PRETO));
+		posicaoPecaNova('f', 8, new Bispo(tabuleiro, XadrezJogador.PRETO));
+		posicaoPecaNova('d', 7, new Peão(tabuleiro, XadrezJogador.PRETO));
+		posicaoPecaNova('e', 7, new Peão(tabuleiro, XadrezJogador.PRETO));
+		posicaoPecaNova('a', 7, new Peão(tabuleiro, XadrezJogador.PRETO));
+		posicaoPecaNova('h', 7, new Peão(tabuleiro, XadrezJogador.PRETO));
+		posicaoPecaNova('b', 7, new Peão(tabuleiro, XadrezJogador.PRETO));
+		posicaoPecaNova('g', 7, new Peão(tabuleiro, XadrezJogador.PRETO));
+		posicaoPecaNova('c', 7, new Peão(tabuleiro, XadrezJogador.PRETO));
+		posicaoPecaNova('f', 7, new Peão(tabuleiro, XadrezJogador.PRETO));
+	}
 
 }
